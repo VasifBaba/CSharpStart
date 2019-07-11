9e2381e2983fa9ea07e98934d206209ff3ceea49
Vasif Baba <51247241+VasifBaba@users.noreply.github.com>
2019-07-11 16:47:45 +0400

    Add files via upload

---
+++ CSharpStart/CSharpStart/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Birinci ededi daaxil edin: ");
+            string a = Console.ReadLine();
+            Console.Write("ikinci ededi daaxil edin: ");
+            string b = Console.ReadLine();
+            int c = Convert.ToInt32(a) * Convert.ToInt32(b);
+            Console.WriteLine("Cavab : " + c);
+            Console.ReadLine();
+            
+
+
+        }
+    }
+}
